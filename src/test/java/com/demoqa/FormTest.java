b919f1f2a7c091f57af772b0d85a48f0fcf4dcc3
mikess840 <mike.dynojet@gmail.com>
2022-08-09 22:21:32 +0500

    Go Master

--- src/test/java/com/demoqa/FormTest.java
+++ src/test/java/com/demoqa/FormTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FormTest {
 
@@ -24,11 +24,17 @@ public class FormTest {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave("Student Registration Form"); // проверяем что страничка подрузилась
+        executeJavaScript("$('footer').remove()"); //убираем баннеры
+        executeJavaScript("$('footer').remove()");
+
 //        $("[id=userName]").setValue("Egor");
         $("#firstName").setValue("Mike");
         $("#lastName").setValue("LolKekovich");
         $("#userEmail").setValue("Tester@tester.com");
-        $("#genterWrapper").$(byText("Male")).click();  //$(".custom-control-label").click()
+        $("#genterWrapper").$(byText("Male")).click(); //ищем внутри блока  //$(".custom-control-label").click()
+        //$("[for=genter-radio-3]").click();
+        //$(by("for","genter-radio-3").click();
         $("#userNumber").setValue("9992223344");
 
         $("#dateOfBirthInput").click();
@@ -41,12 +47,15 @@ public class FormTest {
         $("[for=hobbies-checkbox-1]").click();
         $("[for=hobbies-checkbox-3]").click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/test.jpg"));
+        //$("#uploadPicture").uploadFromClasspath("test.jpg");    //ищет сразу в папке ресурссы
         $("#currentAddress").setValue("Yekaterinburg");
         $("#state").click();
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Karnal")).click();
         $("#submit").click();
+
+        $(".modal-diakit").should(appear); // мониторит что диалог появился
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".modal-body").shouldHave(
@@ -62,6 +71,9 @@ public class FormTest {
         );
         $(".modal-body").shouldHave(text("Haryana Karnal"));
         $(".modal-footer").shouldHave(text("Close"));
+
+        //    $("#table-responsive table").$(byText("Date of Birth")).parent().shouldHave(text("15 August,1990")); - соответствие значений таблицы
+
 
 //        $("#output #name").shouldHave(text("Egor"));
 //        $("#output").$("#name").shouldHave(text("Egor"));
